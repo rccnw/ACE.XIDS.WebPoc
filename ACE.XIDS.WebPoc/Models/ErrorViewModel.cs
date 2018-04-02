9ce8584f3d692c5524e61f311cfec371b7490e65
Rob Campbell <rcc@pobox.com>
2018-04-02 15:41:41 -0700

    Add project files.

---
+++ ACE.XIDS.WebPoc/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace ACE.XIDS.WebPoc.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
